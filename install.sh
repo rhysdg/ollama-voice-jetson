ae98def24943f184a197cf23101986af73b3366a
Rhys Williams <rhys@pronti.ai>
2024-05-31 16:27:10 -0400

    adding whisper base to install script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt-get install python3-pip # If you didn't download pip
 sudo apt install portaudio19-dev
 sudo apt-get install python3-pyaudio
 wget https://developer.download.nvidia.com/compute/redist/jp/v512/pytorch/torch-2.1.0a0+41361538.nv23.06-cp38-cp38-linux_aarch64.whl
+wget https://openaipublic.azureedge.net/main/whisper/models/25a8566e1d0c1e2231d1c762132cd20e0f96a85d16145c3a00adf5d1ac670ead/base.en.pt
 
 pip install -r requirements.txt
 
